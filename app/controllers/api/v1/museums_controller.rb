efaeb5025e6a471ab44dd698cb781dca1b1f8a86
Olivia Bonnal <oliviabonnal@gmail.com>
2022-10-17 18:05:30 +0100

    wrote the logic for getting a response formatted as 'postcode':[museums] when searching by latitude and longitude

--- app/controllers/api/v1/museums_controller.rb
+++ app/controllers/api/v1/museums_controller.rb
@@ -1,3 +1,6 @@
+require "json"
+require "open-uri"
+
 class Api::V1::MuseumsController < ApplicationController
   def index
     @museums = Museum.all
@@ -11,15 +14,39 @@ class Api::V1::MuseumsController < ApplicationController
     when params["postcode"]
       render json: Museum.find_by_param(:postcode, params["postcode"])
     when params["lat"] && params["long"]
-      # call mapbox api to search for lat and long
-      "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=13.437641,52.494857&limit=10&access_token="
-      # select only results where properties[:category] includes "museum"
-      # for each result :
-      # get the name of the museum in "text"
-      # get its postcode in context[0]["text"]
-      # create an instance of Museum
-      # create a hash to show results as "postcode:" [museums]
-      render json: { message: "yay!" }
+      # Call mapbox api to search for lat and long
+      api_key = ENV["API_KEY"]
+      url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=#{params["long"]},#{params["lat"]}&limit=10&access_token=#{api_key}"
+      response = JSON.parse(URI.open(url).read)
+
+      # Mapbox retrieves results which contain "museum" in their name or as a category,
+      # so we select only results which are categorised as "poi" to avoid results like "Museumkwartier" (which is a district in Amsterdam)
+      relevant_results = response["features"].select { |result| result["place_type"].include?("poi") }
+      # and where "museum" is the name of the category in order to avoid results like "Museum Store Products" (which is a business categorised as "poi")
+      relevant_results.select! { |result| result["properties"]["category"].split(", ").include?("museum") }
+
+      # We then initiate a hash to show results as "postcode:" [museums]
+      final_results = {}
+
+      # For each relevant result :
+      relevant_results.each do |result|
+
+        # Get the name of the museum in "text"
+        museum_name = result["text"]
+
+        # Get its postcode in "context", which is an array of hashes
+      # The postcode is not always at the same position in the array so we need to find the hash with an id formatted as "postcode.21630700"
+      # The postcode is then at the "text" key of that hash
+      museum_postcode = result["context"].find { |result| result["id"].split(".").first == "postcode" }["text"]
+
+      # push these results to the hash
+        if final_results.keys.include?(museum_postcode)
+          final_results[museum_postcode].push(museum_name)
+        else
+          final_results[museum_postcode] = [museum_name]
+        end
+      end
+      render json: final_results
     end
   end
 end
