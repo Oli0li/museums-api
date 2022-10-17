d43743b7aa3a684e09148e9b70837ee15148c507
Olivia Bonnal <oliviabonnal@gmail.com>
2022-10-17 11:51:03 +0100

    added dotenv-rails gem

--- app/controllers/api/v1/museums_controller.rb
+++ app/controllers/api/v1/museums_controller.rb
@@ -12,6 +12,7 @@ class Api::V1::MuseumsController < ApplicationController
       render json: Museum.find_by_param(:postcode, params["postcode"])
     when params["lat"] && params["long"]
       # call mapbox api to search for lat and long
+      "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=13.437641,52.494857&limit=10&access_token="
       # select only results where properties[:category] includes "museum"
       # for each result :
       # get the name of the museum in "text"
