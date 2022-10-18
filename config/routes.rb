6e6de23eadb410a570043bbc46f2ba46d18afdfa
Olivia Bonnal <oliviabonnal@gmail.com>
2022-10-18 12:22:34 +0100

    made museums#index the root

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "api/v1/museums#index"
+
   namespace :api do
     namespace :v1 do
       get "/search", to: "museums#search"
-      get "/museums", to: "museums#index"
     end
   end
 end
